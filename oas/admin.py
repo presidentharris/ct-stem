25207e894721344ae93718e85a0b9b8f83a97281
dweintrop <dweintrop@u.northwestern.edu>
2014-01-09 11:41:12 -0600

    added error reporting to guest login and fixed issue w/ student export

--- oas/admin.py
+++ oas/admin.py
@@ -1,4 +1,4 @@
-import csv
+import csv, codecs, cStringIO
 from django.contrib import admin
 # from django.core import serializers
 from django.http import HttpResponse
@@ -124,3 +124,31 @@ admin.site.register(Student, StudentAdmin)
 admin.site.register(AssessEvent, AssessEventAdmin)
 admin.site.register(Response, ResponseAdmin)
 
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode('utf8') if type(s) is unicode else s for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
