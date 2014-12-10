79832b97adc5cdab8488728f187bccef255df2e1
dweintrop <dnw882@dhcp-10-101-71-126.wireless.northwestern.private>
2014-12-10 16:13:54 -0600

    flower pickers initial commit

--- assess/views.py
+++ assess/views.py
@@ -13,6 +13,7 @@ ASSESSMENTS = {
     'CPS1': Assessment(id='CPS1', name='Computational Problem Solving 1', url='cps1.html', current_version='1.0', ipad_compatible=''),
     'HDI': Assessment(id='HDI', name='Human Development Index', url='HDI.html', current_version='1.0', ipad_compatible='disabled'),
     'PB': Assessment(id='PB', name='Purple Bugs', url='purple_bugs.html', current_version='1.0', ipad_compatible='disabled'), 
+    'FP': Assessment(id='FP', name='Flower Pickers', url='flower_pickers.html', current_version='1.0', ipad_compatible='disabled'), 
     'UNI1': Assessment(id='UNI1', name='General CT-STEM Assessment 1', url='uni1.html', current_version='1.0', ipad_compatible='disabled'),
     'WARB': Assessment(id='WARB', name='Warblers', url='warblers.html', current_version='1.0', ipad_compatible=''), 
     # 'UNI2': Assessment(id='UNI2', name='General CT-STEM Assessment 2', url='gen2.html', current_version='1.0', ipad_compatible=''),
